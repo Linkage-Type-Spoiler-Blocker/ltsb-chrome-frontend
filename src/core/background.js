ee54a920b07fef95d4cd57d0acbcf818b2d91d37
백준하 <astoru@naver.com>
2020-05-24 23:10:42 +0900

    implement makeStorageStructure function
    this function will be called when chrome extension is first installed or updated.

---
+++ src/core/background.js
@@ -0,0 +1,22 @@
+function makeStorageStructure() {
+    chrome.storage.sync.set({
+        "excludedSites" : [],
+        "doesPredict" : false,
+        "doesBlockAllContents" : false,
+        "isBlockingEnabled" : false,
+        "userWordsToBeBlocked" : [],
+        "moviesToBeBlocked" : [],
+        "isStorageEnabled" : true
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.storage.sync.get({"isStorageEnabled" : false},function(item){
+        if(item == true){
+            return;
+        }
+        else{
+            makeStorageStructure();
+        }
+    })
+});
