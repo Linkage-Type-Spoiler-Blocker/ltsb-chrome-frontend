6c7e2a4f2e444fc4435698c6287923a6efc06d41
백준하 <astoru@naver.com>
2020-05-26 20:45:24 +0900

    content_script 구조 정의. manifest에 등록

--- src/core/content_script.js
+++ src/core/content_script.js
@@ -1,15 +1,7 @@
-var userOptions;
 
-(function(){
-    chrome.runtime.sendMessage({msg_type : "get"}, function(response) {
-        userOptions = response
-        // console.log(response);
-    });
-})();
-
-//실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
-(function(){
-    chrome.runtime.sendMessage({msg_type : "set", target_obj : {isBlockingEnabled : true}},
-        function(response) {console.log(response);});
+(async () => {
+    const src = chrome.extension.getURL('core/content_scripts/content_main.js');
+    const contentScript = await import(src);
+    contentScript.main();
 })();
 
---
+++ src/core/content_scripts/content_main.js
@@ -0,0 +1,19 @@
+
+var userOptions;
+
+export function main(){
+    (function(){
+        chrome.runtime.sendMessage({msg_type : "get"}, function(response) {
+            userOptions = response
+            // console.log(response);
+        });
+    })();
+
+    //실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
+    (function(){
+        chrome.runtime.sendMessage({msg_type : "set", target_obj : {isBlockingEnabled : true}},
+            function(response) {console.log(response);});
+    })();
+}
+
+
