08ae34977622aab69070dd498eca60909ffc0476
백준하 <astoru@naver.com>
2020-05-28 09:16:33 +0900

    content_main refactoring

--- src/core/content_scripts/content_main.js
+++ src/core/content_scripts/content_main.js
@@ -1,19 +1,30 @@
 
 var userOptions;
 
-export function main(){
-    (function(){
-        chrome.runtime.sendMessage({msg_type : "get"}, function(response) {
-            userOptions = response
-            // console.log(response);
-        });
-    })();
+function loadOptions(){
+    chrome.runtime.sendMessage({msg_type : "get"}, function(response) {
+        userOptions = response;
+        // console.log(response);
+    });
+}
 
-    //실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
-    (function(){
-        chrome.runtime.sendMessage({msg_type : "set", target_obj : {isBlockingEnabled : true}},
-            function(response) {console.log(response);});
-    })();
+
+//실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
+function temp_set_options(){
+    chrome.runtime.sendMessage({msg_type : "set", target_obj : {isBlockingEnabled : true}},
+        function(response) {console.log(response);});
+}
+
+function isBlockingEnabled(){
+    if(userOptions.isBlockingEnabled == true) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+export function main(){
+    loadOptions();
 }
 
 
