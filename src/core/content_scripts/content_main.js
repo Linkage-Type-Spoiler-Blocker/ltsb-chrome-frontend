3974243efb85e5cf8b98573191072fc9f24be060
백준하 <astoru@naver.com>
2020-05-28 17:05:05 +0900

    텍스트만을 추출하는 기능
    article이 아니라 다른 element를 핵심으로 간주할 수 있어야 한다.

--- src/core/content_scripts/content_main.js
+++ src/core/content_scripts/content_main.js
@@ -1,4 +1,6 @@
 
+import {extractFullText} from "./statement_extractor.js";
+
 var userOptions;
 var cur_url;
 
@@ -10,15 +12,11 @@ function loadOptions(){
 }
 
 function loadURL(){
-    // background에 재정의하고, background에 요청하는 기능으로 바꾸기
-    // chrome.tabs.query({active: true, currentWindow : true}, tabs => {
-    //     cur_url = tabs[0].url;
-    // });
     cur_url = location.href;
     console.log(cur_url);
 }
 
-//실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
+// TODO 실제로는 main.html과 options.html에 정의되어야 할 message전송 기능.
 function temp_set_options(){
     chrome.runtime.sendMessage({msg_type : "set", target_obj : {isBlockingEnabled : true}},
         function(response) {console.log(response);});
@@ -34,6 +32,7 @@ function isBlockingEnabled(){
 }
 
 function isExcludedSites(){
+    //TODO 임시제외사이트 뿐만아니라 영구제외사이트도 같이 판단해야함.
     if(userOptions.temp_excluded_sites.includes(cur_url)){
         return true;
     }
@@ -43,9 +42,12 @@ function isExcludedSites(){
 }
 
 export function main(){
+    // TODO 비동기랑 동기가 섞여있음에 유의할것.
+    //기본적으로 자바스크립트 함수가 동기인가? 비동기인가? 함수앞에 명시해야할때가 언제?
     loadOptions();
     loadURL();
-
+    let fullText = extractFullText();
+    console.log(fullText);
 }
 
 
